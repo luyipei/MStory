1208eea913a14d8f1d828c37cd087b994203d695
perper <450283912@qq.com>
2026-02-02 01:18:18 +0800

    init

---
+++ Assets/Scripts/Runtime/Player/PlayerState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runtime.Player
+{
+    public class PlayerState : EntityState
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
